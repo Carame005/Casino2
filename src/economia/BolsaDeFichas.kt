ddf1155fc39e3ced80498c4b4d63a06d8c8843bd
Caramelo <cccarlos@iesrafaelalberti.es>
2025-03-10 08:37:01 +0100

    Eliminada la funcion quitarFichas por ser inutil

--- src/economia/BolsaDeFichas.kt
+++ src/economia/BolsaDeFichas.kt
@@ -13,25 +13,4 @@ class BolsaDeFichas<T : Number> {
     fun totalFichas(): Int {
         return maxOf(fichas.sumOf { (it.valor as? Int) ?: 0 }, 0)
     }
-
-    //Quitar fichas de la bolsa
-    fun quitarFichas(cantidad: Int): Boolean {
-        if (totalFichas() < cantidad) return false
-
-        var restante = cantidad
-        val iterador = fichas.iterator()
-
-        while (iterador.hasNext() && restante > 0) {
-            val ficha = iterador.next()
-            val valorFicha = ficha.valor.toInt()
-
-            if (valorFicha <= restante) {
-                iterador.remove()
-                restante -= valorFicha
-            }
-        }
-
-        return true
-    }
-
 }
