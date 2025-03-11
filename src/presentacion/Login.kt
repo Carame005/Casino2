54db8352a8040d2ab504bdb0f00a6fa51b70d43a
Caramelo <cccarlos@iesrafaelalberti.es>
2025-03-11 07:39:35 +0100

    Implementado un metodo estatico

--- src/presentacion/Login.kt
+++ src/presentacion/Login.kt
@@ -1,19 +1,22 @@
 package presentacion
 
 class Login(private val email: String, private val contraseña: String) {
-    //Expresion regular para validar el correo
-    private val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$".toRegex()
 
     //Validar que ambas condiciones se cumplen
     init {
-        require(validarCorreo()){throw IllegalArgumentException("❌ Correo inválido: $email")}
+        require(validarCorreo(email)){throw IllegalArgumentException("❌ Correo inválido: $email")}
         require(contraseña.isNotEmpty()){throw IllegalArgumentException("⚠ La contraseña no puede estar vacía.")}
         iniciarSesion()
     }
 
-    //Funcion para validar el correo electronico
-    private fun validarCorreo(): Boolean {
-        return email.matches(emailRegex)
+    companion object{
+        //Expresion regular para validar el correo
+        private val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$".toRegex()
+
+        //Metodo estático para validar el correo electronico
+        private fun validarCorreo(email: String): Boolean {
+            return email.matches(emailRegex)
+        }
     }
 
     //Mensaje por pantalla simple para dar a saber que se inició sesion
